4f90a6af1dc4529a046dd49640d3e92b23acd252
Veteroch4k <indefinite05@mail.ru>
2026-04-14 13:53:48 +0700

    Initial commit

---
+++ src/main/java/com/veteroch4k/tasktraker/TaskTimeTrakerApplication.java
@@ -0,0 +1,13 @@
+package com.veteroch4k.tasktraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTimeTrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTimeTrakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veteroch4k/tasktraker/TaskTimeTrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veteroch4k.tasktraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTimeTrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
