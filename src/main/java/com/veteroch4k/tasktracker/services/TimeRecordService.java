f65d5762fcc62948a165bd2f2426cd7d0d75cddf
Veteroch4k <indefinite05@mail.ru>
2026-04-16 21:48:16 +0700

    feat: implement TimeRecordMapper's methods in TimeRecordService

--- src/main/java/com/veteroch4k/tasktracker/services/TimeRecordService.java
+++ src/main/java/com/veteroch4k/tasktracker/services/TimeRecordService.java
@@ -3,6 +3,8 @@ package com.veteroch4k.tasktracker.services;
 import com.veteroch4k.tasktracker.mappers.TimeRecordMapper;
 import com.veteroch4k.tasktracker.models.DTO.TimeRecordRequestDTO;
 import com.veteroch4k.tasktracker.models.TimeRecord;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +14,27 @@ public class TimeRecordService {
 
   private final TimeRecordMapper mapper;
 
-  public void insert(TimeRecordRequestDTO timeRecordDTO) {
+  public TimeRecord createTimeRecord(TimeRecordRequestDTO timeRecordDTO) {
 
     TimeRecord record = new TimeRecord();
     record.setEmployeeId(timeRecordDTO.employeeId());
     record.setTaskId(timeRecordDTO.taskId());
     record.setStartTime(timeRecordDTO.startTime());
     record.setEndTime(timeRecordDTO.endTime());
+    record.setDescription(timeRecordDTO.description());
+
+    mapper.insert(record);
+
+    return record;
+
+  }
+
+  public List<TimeRecord> getStatisticsOfEmployee(Long employeeId, LocalDateTime start, LocalDateTime end) {
+
+    List<TimeRecord> records = mapper.getAllByStartTimeBetween(employeeId, start, end);
+
+    return records;
+
 
   }
 
